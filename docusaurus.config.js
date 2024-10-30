7d117778e960d94b3b438142450cfef8b26107d2
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2024-10-31 01:27:05 +1300

    Optimise homepage style

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,38 +1,184 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
+// // @ts-check
+// // `@type` JSDoc annotations allow editor autocompletion and type checking
+// // (when paired with `@ts-check`).
+// // There are various equivalent ways to declare your Docusaurus config.
+// // See: https://docusaurus.io/docs/api/docusaurus-config
+//
+// import {themes as prismThemes} from 'prism-react-renderer';
+//
+// /** @type {import('@docusaurus/types').Config} */
+// const config = {
+//   title: 'Minimalist Good Post',
+//   tagline: 'Less is More',
+//   favicon: 'img/favicon.ico',
+//
+//   // Set the production url of your site here
+//   url: 'https://your-docusaurus-site.example.com',
+//   // Set the /<baseUrl>/ pathname under which your site is served
+//   // For GitHub pages deployment, it is often '/<projectName>/'
+//   baseUrl: '/',
+//
+//   // GitHub pages deployment config.
+//   // If you aren't using GitHub pages, you don't need these.
+//   organizationName: 'ChanMeng666', // Usually your GitHub org/user name.
+//   projectName: 'minimalist-good-post', // Usually your repo name.
+//
+//   onBrokenLinks: 'throw',
+//   onBrokenMarkdownLinks: 'warn',
+//
+//   // Even if you don't use internationalization, you can use this field to set
+//   // useful metadata like html lang. For example, if your site is Chinese, you
+//   // may want to replace "en" with "zh-Hans".
+//   i18n: {
+//     defaultLocale: 'en',
+//     locales: ['en'],
+//   },
+//
+//   presets: [
+//     [
+//       'classic',
+//       /** @type {import('@docusaurus/preset-classic').Options} */
+//       ({
+//         docs: {
+//           sidebarPath: './sidebars.js',
+//           // Please change this to your repo.
+//           // Remove this to remove the "edit this page" links.
+//           editUrl:
+//             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+//         },
+//         blog: {
+//           showReadingTime: true,
+//           feedOptions: {
+//             type: ['rss', 'atom'],
+//             xslt: true,
+//           },
+//           // Please change this to your repo.
+//           // Remove this to remove the "edit this page" links.
+//           editUrl:
+//             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+//           // Useful options to enforce blogging best practices
+//           onInlineTags: 'warn',
+//           onInlineAuthors: 'warn',
+//           onUntruncatedBlogPosts: 'warn',
+//         },
+//         theme: {
+//           customCss: './src/css/custom.css',
+//         },
+//       }),
+//     ],
+//   ],
+//
+//   themeConfig:
+//     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+//     ({
+//       // Replace with your project's social card
+//       image: 'img/docusaurus-social-card.jpg',
+//       navbar: {
+//         title: 'My Site',
+//         logo: {
+//           alt: 'My Site Logo',
+//           src: 'img/logo.svg',
+//         },
+//         items: [
+//           {
+//             type: 'docSidebar',
+//             sidebarId: 'tutorialSidebar',
+//             position: 'left',
+//             label: 'Tutorial',
+//           },
+//           {to: '/blog', label: 'Blog', position: 'left'},
+//           {
+//             href: 'https://github.com/facebook/docusaurus',
+//             label: 'GitHub',
+//             position: 'right',
+//           },
+//         ],
+//       },
+//       footer: {
+//         style: 'dark',
+//         links: [
+//           {
+//             title: 'Docs',
+//             items: [
+//               {
+//                 label: 'Tutorial',
+//                 to: '/docs/intro',
+//               },
+//             ],
+//           },
+//           {
+//             title: 'Community',
+//             items: [
+//               {
+//                 label: 'Stack Overflow',
+//                 href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+//               },
+//               {
+//                 label: 'Discord',
+//                 href: 'https://discordapp.com/invite/docusaurus',
+//               },
+//               {
+//                 label: 'Twitter',
+//                 href: 'https://twitter.com/docusaurus',
+//               },
+//             ],
+//           },
+//           {
+//             title: 'More',
+//             items: [
+//               {
+//                 label: 'Blog',
+//                 to: '/blog',
+//               },
+//               {
+//                 label: 'GitHub',
+//                 href: 'https://github.com/facebook/docusaurus',
+//               },
+//             ],
+//           },
+//         ],
+//         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+//       },
+//       prism: {
+//         theme: prismThemes.github,
+//         darkTheme: prismThemes.dracula,
+//       },
+//     }),
+// };
+//
+// export default config;
 
+
+
+// docusaurus.config.js
+// @ts-check
 import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Minimalist Good Post',
-  tagline: 'Less is More',
+  title: '极简生活',
+  tagline: '分享简单而美好的生活方式',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ChanMeng666', // Usually your GitHub org/user name.
-  projectName: 'minimalist-good-post', // Usually your repo name.
+  organizationName: 'ChanMeng666',
+  projectName: 'minimalist-good-post',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
   },
+
+  scripts: [
+    {
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/inter-ui/3.19.3/inter.min.css',
+    },
+  ],
 
   presets: [
     [
@@ -41,25 +187,19 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+              'https://github.com/ChanMeng666/minimalist-good-post/tree/main/',
         },
         blog: {
+          blogTitle: '极简生活博客',
+          blogDescription: '分享极简主义生活方式和思考',
+          postsPerPage: 5,
+          blogSidebarCount: 0,
           showReadingTime: true,
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -69,81 +209,58 @@ const config = {
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        image: 'img/social-card.jpg',
+        navbar: {
+          title: '极简生活',
+          items: [
+            {to: '/blog', label: '文章', position: 'right'},
+            {to: '/docs/intro', label: '指南', position: 'right'},
+            {
+              href: 'https://github.com/ChanMeng666/minimalist-good-post',
+              label: 'GitHub',
+              position: 'right',
+            },
+          ],
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+        footer: {
+          style: 'light',
+          links: [
+            {
+              title: '发现',
+              items: [
+                {
+                  label: '所有文章',
+                  to: '/blog',
+                },
+                {
+                  label: '新手指南',
+                  to: '/docs/intro',
+                },
+              ],
+            },
+            {
+              title: '社区',
+              items: [
+                {
+                  label: '豆瓣小组',
+                  href: 'https://douban.com/group/minimalist',
+                },
+                {
+                  label: 'GitHub',
+                  href: 'https://github.com/ChanMeng666/minimalist-good-post',
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} 极简生活`,
+        },
+        prism: {
+          theme: prismThemes.github,
+          darkTheme: prismThemes.dracula,
+        },
+      }),
 };
 
 export default config;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,118 @@
-import clsx from 'clsx';
+// import clsx from 'clsx';
+// import Link from '@docusaurus/Link';
+// import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+// import Layout from '@theme/Layout';
+// import HomepageFeatures from '@site/src/components/HomepageFeatures';
+//
+// import Heading from '@theme/Heading';
+// import styles from './index.module.css';
+//
+// function HomepageHeader() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+//       <div className="container">
+//         <Heading as="h1" className="hero__title">
+//           {siteConfig.title}
+//         </Heading>
+//         <p className="hero__subtitle">{siteConfig.tagline}</p>
+//         <div className={styles.buttons}>
+//           <Link
+//             className="button button--secondary button--lg"
+//             to="/docs/intro">
+//             Docusaurus Tutorial - 5min ⏱️
+//           </Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+//
+// export default function Home() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <Layout
+//       title={`Hello from ${siteConfig.title}`}
+//       description="Description will go into a meta tag in <head />">
+//       <HomepageHeader />
+//       <main>
+//         <HomepageFeatures />
+//       </main>
+//     </Layout>
+//   );
+// }
+
+
+
+// src/pages/index.js
+import React from 'react';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import { ChevronRight } from 'lucide-react';
+import MinimalistCube from './MinimalistCube';
+import styles from './index.module.css';  // 导入CSS Module
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+const FeaturedPost = ({ title, excerpt, link }) => (
+    <Link to={link} className={styles.postCard}>
+        <div className={styles.postContent}>
+            <h3 className={styles.postTitle}>{title}</h3>
+            <p className={styles.postExcerpt}>{excerpt}</p>
+            <div className={styles.readMore}>
+                <span>阅读全文</span>
+                <ChevronRight />
+            </div>
         </div>
-      </div>
-    </header>
-  );
-}
+    </Link>
+);
+
+const MinimalistHeader = () => (
+    <div className={styles.hero}>
+        <div className={styles.heroContent}>
+            <h1 className={styles.heroTitle}>
+                极简生活，<br className={styles.breakLine} />
+                美好生活
+            </h1>
+            <p className={styles.heroSubtitle}>
+                收录豆瓣极简生活小组精华帖，分享极简主义生活方式。让我们一起探索如何通过减法，获得更有质感的生活。
+            </p>
+            <Link to="/blog" className={styles.heroButton}>
+                <span>浏览所有文章</span>
+                <ChevronRight />
+            </Link>
+        </div>
+        <MinimalistCube />
+    </div>
+);
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={siteConfig.title}
+            description="极简生活分享平台"
+        >
+            <main className={styles.main}>
+                <MinimalistHeader />
+                <div className={styles.posts}>
+                    <FeaturedPost
+                        title="如何开始极简生活：新手入门指南"
+                        excerpt="从整理衣柜开始，逐步培养极简思维，让生活回归本质。通过具体的行动步骤，建立属于自己的极简生活方式。"
+                        link="/blog/minimalist-beginner-guide"
+                    />
+                    <FeaturedPost
+                        title="数字极简主义：让科技成为工具而非主宰"
+                        excerpt="学会管理手机使用时间，重新定义与科技的关系。探索如何在数字时代保持专注和高效。"
+                        link="/blog/digital-minimalism"
+                    />
+                    <FeaturedPost
+                        title="极简主义者的居住空间"
+                        excerpt="如何打造一个既实用又富有禅意的生活空间。从空间规划到物品收纳，建立令人心安的居所。"
+                        link="/blog/minimalist-living-space"
+                    />
+                </div>
+            </main>
+        </Layout>
+    );
 }
+
